684b0114b2d472c8cb2821217f1da1ab7f0df4e4
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2025-09-17 12:31:11 +0200

    Refactor portal user ID handling in event information form

--- portal/lets-meet---letsmeetdev/basic-forms/event---information-form-(insert)/Event---Information-Form-(Insert).basicform.custom_javascript.js
+++ portal/lets-meet---letsmeetdev/basic-forms/event---information-form-(insert)/Event---Information-Form-(Insert).basicform.custom_javascript.js
@@ -8,27 +8,21 @@ console.log("Portal User Name: " + portalUserName);
 
 // Wait for the form to be ready
 $(document).ready(function () {
-    console.log("Document ready 3!");
 
-    let idElement = $("#pg_createdbyportaluserid_id");
-    let nameElement = $("#pg_createdbyportaluserid_name");
-    let entityNameElement = $("#pg_createdbyportaluserid_entityname");
-    
+    console.log("Document ready 9!");
+    let attribute = "#pg_createdbyportaluserid";
+    let idElement = $(attribute);
+    let nameElement = $(attribute + "_name");
+    let entityNameElement = $(attribute + "_entityname");
+
     if (idElement && portalUserId) {
         console.log("Field found");
-        // idElement.attr("value", portalUserId);
-        // nameElement.attr("value", portalUserName);
-        // entityNameElement.attr("value", "contact");
-        if (idElement.length && portalUserId) {
-            idElement.val(portalUserId).trigger('change');
-        }
-        if (nameElement.length && portalUserName) {
-            nameElement.val(portalUserName).trigger('change');
-        }
-        if (entityNameElement.length) {
-            entityNameElement.val('contact').trigger('change');
-        }
+        idElement.attr("value", portalUserId);
+        nameElement.attr("value", portalUserName);
+        entityNameElement.attr("value", "contact");
+
         console.log("Set portal user ID: " + portalUserId);
+
     } else {
         console.log("Field not found or portalUserId is empty");
     }
