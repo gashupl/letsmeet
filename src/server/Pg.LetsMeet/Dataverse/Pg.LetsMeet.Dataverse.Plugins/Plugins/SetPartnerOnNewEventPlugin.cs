b4acc1c08c95fa96a2570d60df25c326261d3683
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2025-09-18 08:44:02 +0200

    Add SetPartnerOnNewEventPlugin and its tests

---
+++ src/server/Pg.LetsMeet/Dataverse/Pg.LetsMeet.Dataverse.Plugins/Plugins/SetPartnerOnNewEventPlugin.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xrm.Sdk;
+using Pg.LetsMeet.Dataverse.Common.Values;
+using Pg.LetsMeet.Dataverse.Context;
+using Pg.LetsMeet.Dataverse.Domain;
+using Pg.LetsMeet.Dataverse.Shared.Injections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg.LetsMeet.Dataverse.Plugins.Plugins
+{
+    public class SetPartnerOnNewEventPlugin : PluginBase
+    {
+        public override IDependencyLoader DependencyLoader { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override bool IsContextValid(IPluginExecutionContext pluginExecutionContext)
+        {
+            if (pluginExecutionContext?.Mode == (int)ProcessingMode.Synchronous
+                   && pluginExecutionContext?.PrimaryEntityName == pg_event.EntityLogicalName
+                   && pluginExecutionContext.MessageName == MessageName.Create
+                   && pluginExecutionContext.Stage == (int)ProcessingStage.PreOperation)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void Execute(IPluginExecutionContext pluginExecutionContext, IServicesFactory servicesFactory, ITracingService tracingService)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/server/Pg.LetsMeet/Test/Dataverse/Pg.LetsMeet.Dataverse.Plugins.Tests/Plugins/SetPartnerOnNewEventPluginTests.cs
@@ -0,0 +1,48 @@
+﻿using FakeXrmEasy;
+using Pg.LetsMeet.Dataverse.Common.Values;
+using Pg.LetsMeet.Dataverse.Context;
+using Pg.LetsMeet.Dataverse.Plugins.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pg.LetsMeet.Dataverse.Plugins.Tests.Plugins
+{
+    public class SetPartnerOnNewEventPluginTests
+    {
+        [Fact]
+        public void IsContextValid_ValidContext_ReturnsTrue()
+        {
+            var pluginContext = new XrmFakedPluginExecutionContext()
+            {
+                PrimaryEntityName = pg_event.EntityLogicalName,
+                MessageName = MessageName.Create,
+                Mode = (int)ProcessingMode.Synchronous, 
+                Stage = (int)ProcessingStage.PreOperation
+            };
+
+            var plugin = new SetPartnerOnNewEventPlugin();
+            var isValid = plugin.IsContextValid(pluginContext);
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void IsContextValid_ValidContext_ReturnsFalse()
+        {
+            var pluginContext = new XrmFakedPluginExecutionContext()
+            {
+                PrimaryEntityName = pg_event.EntityLogicalName,
+                MessageName = MessageName.Update,
+                Mode = (int)ProcessingMode.Synchronous,
+                Stage = (int)ProcessingStage.PreOperation
+            };
+
+            var plugin = new SetPartnerOnNewEventPlugin();
+            var isValid = plugin.IsContextValid(pluginContext);
+            Assert.False(isValid);
+        }
+    }
+}
