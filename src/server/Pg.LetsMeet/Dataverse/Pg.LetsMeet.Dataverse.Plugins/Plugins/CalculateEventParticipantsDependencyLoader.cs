1ca30badeba7990ea17b29adc14e5c6b212bbc63
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2025-09-22 08:50:16 +0200

    Refactor using directives in CalculateEventParticipantsDependencyLoader
    Removed unnecessary using directives and added Pg.LetsMeet.Dataverse.Infrastructure to streamline dependencies and improve code structure.

--- src/server/Pg.LetsMeet/Dataverse/Pg.LetsMeet.Dataverse.Plugins/Plugins/CalculateEventParticipantsDependencyLoader.cs
+++ src/server/Pg.LetsMeet/Dataverse/Pg.LetsMeet.Dataverse.Plugins/Plugins/CalculateEventParticipantsDependencyLoader.cs
@@ -1,10 +1,8 @@
 ﻿using Pg.LetsMeet.Dataverse.Domain.DataAccess;
 using Pg.LetsMeet.Dataverse.Infrastructure;
-using Pg.LetsMeet.Dataverse.Domain;
 using Pg.LetsMeet.Dataverse.Domain.BusinessLogic.Demo;
 using Pg.LetsMeet.Dataverse.Domain.BusinessLogic.Environment;
 using Pg.LetsMeet.Dataverse.Domain.BusinessLogic.EventParticipations;
-using Microsoft.Xrm.Sdk;
 using Pg.LetsMeet.Dataverse.Shared.Injections;
 using System;
 
