6a36ec69fb98bf001fb2ac99e06a356e147436a9
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2026-01-15 07:55:22 +0100

    Updating home page

--- portal/lets-meet---letsmeetdev/basic-forms/event---information-form-(insert)/Event---Information-Form-(Insert).basicform.custom_javascript.js
+++ portal/lets-meet---letsmeetdev/basic-forms/event---information-form-(insert)/Event---Information-Form-(Insert).basicform.custom_javascript.js
@@ -9,6 +9,12 @@ console.log("Portal User Name: " + portalUserName);
 // Wait for the form to be ready
 $(document).ready(function () {
 
+    let  removeTextMuted = (attributeName) => {
+        let field = $(`#${attributeName}_name`).parent();
+        let textmuted = field.find('.text-muted');
+        textmuted.remove(); 
+    };
+
     console.log("Document ready 9!");
     let attribute = "#pg_createdbyportaluserid";
     let idElement = $(attribute);
@@ -18,11 +24,12 @@ $(document).ready(function () {
     if (idElement && portalUserId) {
         console.log("Field found");
         idElement.attr("value", portalUserId);
+       // nameElement.val(portalUserName); 
         nameElement.attr("value", portalUserName);
         entityNameElement.attr("value", "contact");
+        //removeTextMuted (attribute); 
 
         console.log("Set portal user ID: " + portalUserId);
-
     } else {
         console.log("Field not found or portalUserId is empty");
     }
