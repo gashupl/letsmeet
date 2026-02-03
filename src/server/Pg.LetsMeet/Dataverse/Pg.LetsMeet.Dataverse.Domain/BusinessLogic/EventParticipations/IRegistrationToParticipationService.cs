8b4bfb7ecc56eb19d843760cb30f43541dc5abcc
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2026-02-03 08:35:20 +0100

    Add methods to IRegistrationToParticipationService interface

--- src/server/Pg.LetsMeet/Dataverse/Pg.LetsMeet.Dataverse.Domain/BusinessLogic/EventParticipations/IRegistrationToParticipationService.cs
+++ src/server/Pg.LetsMeet/Dataverse/Pg.LetsMeet.Dataverse.Domain/BusinessLogic/EventParticipations/IRegistrationToParticipationService.cs
@@ -1,6 +1,12 @@
-﻿namespace Pg.LetsMeet.Dataverse.Domain.BusinessLogic.EventParticipations
+﻿using Pg.LetsMeet.Dataverse.Context;
+using System;
+
+namespace Pg.LetsMeet.Dataverse.Domain.BusinessLogic.EventParticipations
 {
     public interface IRegistrationToParticipationService : IService
     {
+        CreateParticipationsFromRegistrationsResult TryCreateParticipationsFromRegistrations(pg_eventregistrationform registrationForm);
+
+        void CloseRegistrationForm(Guid registrationFormId, CreateParticipationsFromRegistrationsResult convertionResult); 
     }
 }
