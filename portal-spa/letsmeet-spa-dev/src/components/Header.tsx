0ccc798754c7a0d0302a9f1d8c50dd729b2a6374
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2026-04-15 09:25:39 +0200

    Power pages portal configuration

--- portal-spa/letsmeet-spa-dev/src/components/Header.tsx
+++ portal-spa/letsmeet-spa-dev/src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
         <div className="header-content">
           <Link to="/" className="logo">
             <span className="logo-icon">📅</span>
-            <span className="logo-text">Let's Meet!</span>
+            <span className="logo-text">Let's Meet! (v.3)</span>
           </Link>
           
           <nav className="nav">
